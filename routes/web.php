afeb11c6f832ac29fcfaeb87ead798c632339efc
Erwin Angeles <erwin@MacBook-Pro.local>
2021-07-23 16:45:58 -0700

    created routes for AuthController login register and user profile

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('login', 'AuthController@login')->name('login');
+Route::post('login', 'AuthController@postLogin')->name('auth.postLogin');
+
+Route::get('register', 'AuthController@register')->name('register');
+Route::post('register', 'AuthController@postRegister')->name('auth.postRegister');
+Route::post('logout', 'AuthController@logout')->name('logout');
+
+Route::get('user/profile', 'AuthController@profile')->name('user.profile');
