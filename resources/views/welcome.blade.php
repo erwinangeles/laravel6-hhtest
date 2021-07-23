953a3ae22851a0e5fa370f905068244e55aa2dc3
Erwin Angeles <erwin@MacBook-Pro.local>
2021-07-23 16:44:14 -0700

    added instructions for project on welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,27 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
-</html>
+@extends('layouts.app')
+@section('body')
+<div class="container bg-light" style="margin-top: 30px; padding: 20px;">
+    <p><strong>Using Laravel 6 (since it is the current LTS release) implement the following:</strong></p>
+    <ul>
+    <li>A user system with authentication and registration controllers/views</li>
+    </ul>
+    <ul>
+    <ul>
+    <li>Include a separate user_attributes table that allows a user to enter and store their birthday, gender, and country</li>
+    </ul>
+    </ul>
+    <ul>
+    <li>A user profile page that can only be viewed after logging in that lets them view and update their login details and attributes</li>
+    <li>A console command that will export users and their attributes to a CSV when run</li>
+    <li>Exposes a REST API that can be used to validate user credentials and get/update their attributes with authorized API key(s)</li>
+    </ul>
+    <ul>
+    <ul>
+    <li>API key(s) can be a single key from pulled from an environment variable or a table in the DB that can store multiple keys, your choice</li>
+    </ul>
+    </ul>
+    <ul>
+    <li>PHPUnit tests that provide code coverage for all the custom written code</li>
+    </ul>
+</div>
+@endsection
