67644906dc63ade1491efb3672652b03140a5c12
Erwin Angeles <erwin@MacBook-Pro.local>
2021-07-23 16:46:30 -0700

    created AuthController to manage register, login and profile routes

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+Use App\User;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(){
+        return view('users.auth.login');
+    }
+
+    public function register(){
+        return view('users.auth.register');
+    }
+
+    public function postLogin(Request $request){
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        $credentials = $request->only('email', 'password');
+
+        if(Auth::attempt($credentials)) {
+            return redirect()->intended('user/profile');
+        }
+
+        $error = "These credentials do not match our records.";
+        return back()->withErrors($error);
+    }
+
+    public function postRegister(Request $request){
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8|confirmed'
+        ]);
+        
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+        return redirect()->route('login');
+    }
+
+    public function logout(){
+        Auth::logout();
+        return redirect()->route('login');
+    }
+
+    public function profile(){
+        return view('users.profile');
+    }
+}
