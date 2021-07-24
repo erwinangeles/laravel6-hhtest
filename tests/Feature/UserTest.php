84676a9448d63928697634986d84c935e844f67e
Erwin Angeles <erwin@MacBook-Pro.local>
2021-07-23 18:13:06 -0700

    added test for test_only_logged_in_users_can_see_profile_page and test_user_updates_details_on_profile , plus small tweak to assertion when creating a new account

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -27,7 +27,8 @@ class UserTest extends TestCase
         ]);
 
         //Verifying user was created
-        $this->assertEquals(1, User::where('email', '=', 'erwin@example.com')->count());
+        $this->assertTrue(User::where('email', '=', 'erwin@example.com')->exists());
+
 
         //Attempt to login using the email and password of the recently created user
         $response = $this->post('/login', [
@@ -37,6 +38,31 @@ class UserTest extends TestCase
 
         //Check that user logged in status is true after posting login
         $this->assertTrue(Auth::check());
+    }
+
+    public function test_only_logged_in_users_can_see_profile_page(){
+        //verify user is redirected if not logged in
+        $response = $this->get('/user/profile')->assertRedirect('login');
+    }
+
+    public function test_authenticated_users_can_see_profile_page(){
+        //creates dummy user to use profile
+        $this->actingAs(factory(User::class)->create());
+        $response = $this->get('/user/profile')->assertOk();
+    }
+
+    public function test_user_updates_details_on_profile(){
+        //uses dummy user to update
+        $this->actingAs($user = factory(User::class)->create())->post('/user/update', [
+            'name' => 'Sally Sue',
+            'password' => 'Password123',
+            'password_confirmation' => 'Password123',
+            'email' => 'updated@example.com'
+        ])->assertOk();
+
+        //verifying user was updated
+        $this->assertTrue(User::where('email', '=', 'updated@example.com')->exists());
+
     }
 
 }
