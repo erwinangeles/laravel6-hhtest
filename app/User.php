02ea8659eb7ecf34473652169b690f1401578c28
Erwin Angeles <erwin@MacBook-Pro.local>
2021-07-25 18:23:53 -0700

    added attributes public function to pull data from user_attributes table

--- app/User.php
+++ app/User.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\DB;
 
 class User extends Authenticatable
 {
@@ -36,4 +37,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function attributes(){
+        return DB::table('user_attributes')->where('user_id', $this->id)->get();
+    }
+
 }
